039878f031c0c7311493641a1dbd3433119f8a74
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2019-10-08 18:33:39 -0500

    Menus Started
    Monster Mash dat biiiiichhh

---
+++ UnityGame/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public GameObject firstPersonGroup;
+    public GameObject MainMenuGroup;
+    public GameObject MainMenuScreen;
+    public GameObject MainMenuOptions;
+
+    public Button startButton;
+    public Button optionsButton;
+
+
+    void Start()
+    {
+        startButton.onClick.AddListener(gameStart);
+        optionsButton.onClick.AddListener(optionMenu);
+    }
+
+    void gameStart()
+    {
+        MainMenuGroup.SetActive(false);
+        firstPersonGroup.SetActive(true);
+    }
+
+    void optionMenu()
+    {
+        MainMenuScreen.SetActive(false);
+        MainMenuOptions.SetActive(true);
+
+    }
+
+}
---
+++ UnityGame/Assets/Scripts/MenuCameraRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraRotate : MonoBehaviour
+{
+
+    public float rotSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotSpeed, 0, Space.Self);
+
+    }
+}
