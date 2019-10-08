ba81e4e121e0b705cd5a24181455a5730a43b57d
Conner <clmeriweather@gmail.com>
2019-10-08 18:21:33 -0500

    Killbox code

---
+++ UnityGame/Assets/Scripts/KillBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+	void OnTriggerEnter(Collider obj)
+	{
+		if(obj.tag == "Player")
+		{
+			SceneManager.LoadScene("MainScene");
+		}
+	}
+}
