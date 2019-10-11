2de0f6fa3680c4ce470febdb36136e2cc1287027
Conner <clmeriweather@gmail.com>
2019-10-11 15:14:23 -0500

    Kill boxes

--- UnityGame/Assets/Scripts/KillBox.cs
+++ UnityGame/Assets/Scripts/KillBox.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 
 public class KillBox : MonoBehaviour
 {
+	public GameObject player;
+	public Vector3 startPos;
+	public Quaternion startRot;
+
+	void Update()
+	{
+		if (player == null)
+		{
+			player = GameObject.FindGameObjectWithTag("Player");
+			if (player != null)
+			{
+				startPos = player.transform.position;
+				startRot = player.transform.rotation;
+			}
+		}
+	}
+
 	void OnTriggerEnter(Collider obj)
 	{
+		
 		if(obj.tag == "Player")
 		{
-			SceneManager.LoadScene("MainScene");
+			Debug.Log("HERE");
+			player.transform.position = startPos;
+			player.transform.rotation = startRot;
 		}
 	}
 }
