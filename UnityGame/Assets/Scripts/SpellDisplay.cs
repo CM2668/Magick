334583c9eaf0286ed62b817bcafc89c3194c7a6b
Conner <clmeriweather@gmail.com>
2019-11-09 01:24:55 -0600

    hmm

--- UnityGame/Assets/Scripts/SpellDisplay.cs
+++ UnityGame/Assets/Scripts/SpellDisplay.cs
@@ -399,7 +399,7 @@ public class SpellDisplay : MonoBehaviour
                     displayText = "";
                     break;
             }
-            //Clear();
+			RemoveRunes();
         }
 	}
 
@@ -497,25 +497,30 @@ public class SpellDisplay : MonoBehaviour
         hasteUI.SetActive(false);
         telekinesisUI.SetActive(false);
 
-        if (newRune1 != null)
-        {
-            GameObject.Destroy(newRune1);
-            newRune1 = null;
-        }
-        if (newRune2 != null)
-        {
-            GameObject.Destroy(newRune2);
-            newRune2 = null;
-        }
-        if (newRune3 != null)
-        {
-            GameObject.Destroy(newRune3);
-            newRune3 = null;
-        }
+		RemoveRunes();
 
         displayText = "";
         spellTarget = null;
         activeSpell = null;
     }
+
+	public void RemoveRunes()
+	{
+		if (newRune1 != null)
+		{
+			GameObject.Destroy(newRune1);
+			newRune1 = null;
+		}
+		if (newRune2 != null)
+		{
+			GameObject.Destroy(newRune2);
+			newRune2 = null;
+		}
+		if (newRune3 != null)
+		{
+			GameObject.Destroy(newRune3);
+			newRune3 = null;
+		}
+	}
 }
 
